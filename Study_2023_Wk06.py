e904ecf2c52500c0ec5180f5bb90212f18c9c5d8
gogongkong <zorek2@gmail.com>
2023-02-12 02:16:04 +0900

    PythonStudy_0212
    실전문제 : 게임개발 문제 이해 될때까지 이미 푼 문제와 복습만 진행할 예정

--- Study_2023_Wk06.py
+++ Study_2023_Wk06.py
@@ -667,14 +667,19 @@ n, k = map(int, input().split())
 result = 0
 while n!=1:
     if n %k !=0: 
-        i = n%k
-        n -=i
-        result += i
+        result += n%k
+        n -= n%k
+        # i = n%k
+        # n -=i
+        # result += i
     elif n%k ==0:
         n /=k
         result +=1
 
 print(result)
+n,k=map(int,input().split())
+answer=n//k+ n%k 
+print(answer)
 
 ''' 구현 예제 4-1 상하좌우 110p '''
 n = int(input())
@@ -722,3 +727,106 @@ for step in steps:
         result +=1
 
 print(result)
+
+''' 2월 11일 공부 시작'''
+
+어려웠던 실전문제2 115p 왕실의 나이트 다시 풀어보기
+
+풀이 방법
+1. 전체 이동 경우의 수를 만들기
+2. 이동 방법을 입력 받고 못가는 경우의 수를 확인 후 해당 경우의 수를 빼기
+
+move = input()
+move_row = int(move[1])
+move_colum = int(ord(move[0])) - int(ord('a')) +1
+steps = [(2,1), (2,-1), (-2,1), (1,2), (-1,2), (1,-2),(-2,-1),(-1,-2)]
+result = 0
+
+for step in steps:
+    next_row = move_row + step[0]
+    next_colum = move_colum + step[1]
+
+    if next_row >= 1 and next_row <= 8 and next_colum >=1 and next_colum <= 8:
+        result += 1
+
+print(result)
+
+''' 실전문제3 게임개발 118p'''
+# N, M을 공백으로 구분하여 입력받기
+n, m = map(int, input().split())
+
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+# 현재 캐릭터의 X좌표, Y좌표, 방향을 입력받기
+x, y, direction = map(int, input().split())
+d[x][y] = 1 # 현재 좌표 방문 처리
+
+# 전체 맵 정보를 입력받기
+array = []
+for i in range(n):
+    array.append(list(map(int, input().split())))
+
+# 북, 동, 남, 서 방향 정의
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# 왼쪽으로 회전
+def turn_left():
+    global direction
+    direction -= 1
+    if direction == -1:
+        direction =3
+    
+#시뮬레이션 시작
+count = 1
+turn_time = 0
+while True:
+    #왼쪽으로 회전
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    # 회전한 이후 정면에 가보지 않은 칸이 존재하는 경우 이동
+    if d[nx][ny] == 0 and array[nx][ny] == 0:
+        d[nx][ny] =1
+        x = nx
+        y = ny
+        count += 1
+        turn_time = 0
+        continue
+        # 회전한 이후 정면에 가보지 않은 칸이 없거나 바다인 경우
+    else:
+        turn_time += 1
+    # 네 방향 모두 갈 수 없는 경우 
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+        # 뒤로 갈 수 있다면 이동하기
+        if array[nx][ny] == 0:
+            x = nx
+            y = ny
+        # 뒤가 바다로 막혀있는 경우
+        else:
+            break
+        turn_time = 0
+
+print(count)
+
+move_step = int(input())
+direction = input().split()
+x,y = 1, 1
+result = 0
+
+dy = [-1, 1, 0, 0]
+dx = [0, 0, -1, 1]
+move = ['L', 'R', 'U', 'D']
+
+for i in direction:
+    for j in range(len(move)):
+        if i == move[j]:
+            nx = x +dx[j]
+            ny = y +dy[j]
+    if nx <1 or nx >move_step or ny<1 or ny>move_step:
+        continue
+    x, y = nx, ny
+
+print(x, y)
