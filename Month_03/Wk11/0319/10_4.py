299ea7afe8272e785b12309a7b8a7eacbbeb36d5
gogongkong <zorek2@gmail.com>
2023-03-19 19:19:15 +0900

    PythonStudy_20230319
    앞으로 할일
    10_4 질문하기
    남은 문제풀기

--- Month_03/Wk11/0319/10_4.py
+++ Month_03/Wk11/0319/10_4.py
@@ -57,7 +57,7 @@ graph = [[] for i in range(v+1)]
 # 각 강의 시간을 0으로 초기화
 time = [0] * (v+1)
 
-# 방향그래프의 모든 간선 정보를 입력바딕
+# 방향그래프의 모든 간선 정보를 입력받기
 for i in range(1, v+1):
     data = list(map(int, input().split()))
     time[i] = data[0] # 첫 번째 수는 시간 정보를 담고 있음
