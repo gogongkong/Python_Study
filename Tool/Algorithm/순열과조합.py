f2589002e36c76f8ad6bcde9f343ce24ce8c35b3
gogongkong <zorek2@gmail.com>
2023-04-05 23:52:12 +0900

    PythonStudy_20230405
    내일할일
    새로운 마음가짐으로 이론공부 및 이론에 맞는 유형의 문제를 2~3문제씩은 풀어보자
    할수있다!

--- Tool/Algorithm/순열과조합.py
+++ Tool/Algorithm/순열과조합.py
@@ -7,15 +7,18 @@ nPr이라고 표현
 다만 코딩테스트에서는 해당 경우의 수의 값만 출력하는것이 아닌 모든 경우(사건)을 출력해야 하기도 함
 이럴때 사용하는 함수가 "itertools"라이브러리 이다
 permutations() 함수를 호출해 나온 결과는 리스트의 형태가 아니기 때문에 list()를 사용하자
+순열은 순서를 고려하기 때문에 [A, B, C]의 리스트에서 2개의 원소를 골라 순서를 정해 나열하면
+[(A, B), (A, C), (B, A), (B, C), (C, A), (C, B)] 가 나오게 된다. 즉 순열에서는 (A, B)와 (B, A)는 다른 것이다.
 '''
 
 # 소스코드
 import itertools
 
-data = [1, 2]
+arr = ['A', 'B', 'C']
+nPr = itertools.permutations(arr, 2)
+print(list(nPr))
 
-for x in itertools.permutations(data, 2):
-    print(list(x))
+#결과 : [('A', 'B'), ('A', 'C'), ('B', 'A'), ('B', 'C'), ('C', 'A'), ('C', 'B')]
 
 
 
@@ -25,12 +28,15 @@ for x in itertools.permutations(data, 2):
 nCr이라고 표현
 --> nCr = n! / r! * (n-r)!
 조합 라이브러리의 이름은 itertools의 combinations()
+조합은 순서를 고려하지 않기 때문에 [A, B, C]의 리스트에서 2개의 원소를 골라 나열하면
+[(A, B), (A, C), (B, C)] 가 나오게 된다. 조합은 (A, B)와 (B, A)는 같은 것으로 취급한다.
 '''
 
 # 소스코드
 import itertools
 
-data = [1, 2, 3]
+arr = ['A', 'B', 'C']
+nCr = itertools.combinations(arr, 2)
+print(list(nCr))
 
-for x in itertools.combinations(data, 2):
-    print(list(x), end=' ')
+#결과 : [('A', 'B'), ('A', 'C'), ('B', 'C')]
