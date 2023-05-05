64bc11f0e122b12600be28de50deb24de73915af
gogongkong <zorek2@gmail.com>
2023-05-05 23:20:58 +0900

    PythonStudy_20230503~20230505
    다음할일
    숫자카드2 꼭 풀어보기
    딕셔너리 함수 공부해보기
    이진탐색 문제 실버2까지 풀어보기

---
+++ Month_04/Wk18/0505/백준10815번_숫자카드_S5_재귀함수.py
@@ -0,0 +1,50 @@
+'''
+https://www.acmicpc.net/problem/10815
+
+문제
+숫자 카드는 정수 하나가 적혀져 있는 카드이다. 
+상근이는 숫자 카드 N개를 가지고 있다. 
+정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 가지고 있는지 아닌지를 구하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 상근이가 가지고 있는 숫자 카드의 개수 N(1 ≤ N ≤ 500,000)이 주어진다. 
+둘째 줄에는 숫자 카드에 적혀있는 정수가 주어진다. 
+숫자 카드에 적혀있는 수는 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다. 
+두 숫자 카드에 같은 수가 적혀있는 경우는 없다.
+
+셋째 줄에는 M(1 ≤ M ≤ 500,000)이 주어진다. 
+넷째 줄에는 상근이가 가지고 있는 숫자 카드인지 아닌지를 구해야 할 M개의 정수가 주어지며, 
+이 수는 공백으로 구분되어져 있다. 이 수도 -10,000,000보다 크거나 같고, 10,000,000보다 작거나 같다
+
+출력
+첫째 줄에 입력으로 주어진 M개의 수에 대해서, 
+각 수가 적힌 숫자 카드를 상근이가 가지고 있으면 1을, 아니면 0을 공백으로 구분해 출력한다.
+
+예제 입력 1 
+5
+6 3 2 10 -10
+8
+10 9 -5 2 3 4 5 -10
+예제 출력 1 
+1 0 0 1 1 0 0 1
+'''
+
+n = int(input())
+data = list(map(int, input().split()))
+data.sort() # -10, 2, 3, 6, 10
+m = int(input())
+data_r = list(map(int, input().split()))
+
+def search(data, target, start, end):
+    if start > end:
+        return print(0,end=' ')
+    mid = (start + end) // 2
+    if data[mid] == target:
+        return print(1,end=' ')
+    elif data[mid] < target:
+        search(data, target, mid + 1, end)
+    else:
+        search(data, target, start, mid - 1)
+
+for target in data_r:
+    search(data, target, 0, n-1)
---
+++ Reference/이진탐색/이진탐색연습문제_부품찾기_이진탐색_반복문.py
@@ -0,0 +1,63 @@
+'''
+동빈이네 전자 매장에는 부품이 N개 있다.
+각 부품은 정수 형태의 고유한 번호가 있다.
+어느날 손님이 M개 종류의 부품을 대향으로 구매하겠다며 당일날 견적서를 요청했다.
+동빈이는 때를 놓치지 않고 손님이 문의한 부품 M개 종류를 모두 확인해서 견적서를 작성해야 한다.
+이때 가게 안에 부품이 모두 있는지 확인하는 프로그램을 작성해보자.
+예를 들어 가게의 부품이 총 5개일 때 부품 번호가 다음과 같다고 하자.
+    N = 5
+    [8, 3, 7, 9, 2]
+
+손님은 총 3개의 부품이 있는지 확인 요청했는데 부품 번호는 다음과 같다
+    M = 3
+    [5, 7, 9]
+
+이때 손님이 요청한 부품 번호의 순서대로 부품을 확인해 부품이 있으면 yes를, 없으면 no를 출력한다.
+구분은 공백으로 한다.
+
+입력 조건
+    - 첫째 줄에 정수 N이 주어진다. (1 <= N <= 1,000,000)
+    - 둘재 줄에는 공백으로 구분하여 N개의 정수가 주어진다. 이때 정수는 1보다 크고 1,000,000 이하이다.
+    - 셋째 줄에는 정수 M이 주어진다. (1 <= M <= 100,000)
+    - 넷째 줄에는 공백으로 구분하여 M개의 정수가 주어진다. 이때 정수는 1보다 크고 1,000,000 이하이다.
+
+출력 조건
+    - 첫째 줄에 공백으로 구분하여 각 부품이 존재하면 yes를, 없으면 no를 출력한다.
+
+입력 예시
+5
+8 3 7 9 2
+3
+5 7 9
+
+출력 예시
+no yes yes
+'''
+
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+m = int(input())
+data_cus = list(map(int, input().split()))
+
+# 이진 탐색 ( 반복문 )
+def binary_search_for(data, target, start, end):
+    while True:
+        if start > end:
+            return print('no', end=' ')
+        mid = (start + end) // 2
+        if data[mid] == target:
+            return print('yes',end=' ')
+        elif data[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+for i in data_cus:
+    binary_search_for(data, i, 0, n-1)
+
+
+
+
+    
+
