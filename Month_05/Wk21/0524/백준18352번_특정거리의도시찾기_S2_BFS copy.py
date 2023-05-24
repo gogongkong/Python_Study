7179228e009e74f8c0d778a77223b3ecadf0d989
gogongkong <zorek2@gmail.com>
2023-05-24 23:53:06 +0900

    PythonStudy_20230524
    다음할일
    그래프이론 공부
    18352번 문제 다시 풀기(두가지방법 모두)

---
+++ Month_05/Wk21/0524/백준18352번_특정거리의도시찾기_S2_BFS copy.py
@@ -0,0 +1,76 @@
+'''
+https://www.acmicpc.net/problem/18352
+어떤 나라에는 1번부터 N번까지의 도시와 M개의 단방향 도로가 존재한다. 모든 도로의 거리는 1이다.
+
+이 때 특정한 도시 X로부터 출발하여 도달할 수 있는 모든 도시 중에서, 
+최단 거리가 정확히 K인 모든 도시들의 번호를 출력하는 프로그램을 작성하시오. 
+또한 출발 도시 X에서 출발 도시 X로 가는 최단 거리는 항상 0이라고 가정한다.
+입력
+첫째 줄에 도시의 개수 N, 도로의 개수 M, 거리 정보 K, 출발 도시의 번호 X가 주어진다. 
+(2 ≤ N ≤ 300,000, 1 ≤ M ≤ 1,000,000, 1 ≤ K ≤ 300,000, 1 ≤ X ≤ N) 
+둘째 줄부터 M개의 줄에 걸쳐서 두 개의 자연수 A, B가 공백을 기준으로 구분되어 주어진다. 
+이는 A번 도시에서 B번 도시로 이동하는 단방향 도로가 존재한다는 의미다. (1 ≤ A, B ≤ N) 단, A와 B는 서로 다른 자연수이다.
+
+출력
+X로부터 출발하여 도달할 수 있는 도시 중에서, 최단 거리가 K인 모든 도시의 번호를 한 줄에 하나씩 오름차순으로 출력한다.
+이 때 도달할 수 있는 도시 중에서, 최단 거리가 K인 도시가 하나도 존재하지 않으면 -1을 출력한다.
+
+예제 입력 1 
+4 4 2 1
+1 2
+1 3
+2 3
+2 4
+예제 출력 1 
+4
+예제 입력 2 
+4 3 2 1
+1 2
+1 3
+1 4
+예제 출력 2 
+-1
+예제 입력 3 
+4 4 1 1
+1 2
+1 3
+2 3
+2 4
+예제 출력 3 
+2
+3
+'''
+from collections import deque
+
+n, m, find, start = map(int, input().split())
+distance = [0] * (n+1)
+visited = [False] * (n+1)
+
+data = [[] for _ in range(n+1)]
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    data[a].append(b)
+
+def bfs(start):
+    queue = deque()
+    queue.append(start)
+    visited[start] = True
+    result = []
+    while queue:
+        now = queue.popleft()
+        for i in data[now]:
+            if not visited[i]:
+                visited[i] = True
+                distance[i] = distance[now] + 1
+                queue.append(i)
+                if distance[i] == find:
+                    result.append(i)
+    if len(result) == 0:
+        print(-1)
+    else:
+        result.sort()
+        for i in result:
+            print(i)
+
+bfs(start)
