4d65484bc7e565ee2ca229daa9610f554cf7cb67
gogongkong <zorek2@gmail.com>
2023-05-29 17:26:56 +0900

    PythonStudy_20230529
    오늘은 여기까지

--- Month_05/Wk21/0528/11_Q4(316p)_만들수없는금액.py
+++ Month_05/Wk21/0528/11_Q4(316p)_만들수없는금액.py
@@ -24,33 +24,17 @@
 8
 '''
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#풀이
 n = int(input())
+
 data = list(map(int, input().split()))
 data.sort()
 
 target = 1
-for x in data:
-    # 만들 수 없는 금액을 찾았을 때 반복 종료
-    if target < x:
-        break
-    target += x
 
-# 만들 수 없는 금액 출력
+for i in data:
+    if target < i:
+        break
+    else:
+        target += i
+
 print(target)
