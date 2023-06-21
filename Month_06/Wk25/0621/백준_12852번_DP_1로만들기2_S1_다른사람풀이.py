0489640da0d3d89578672dec2956ff34d1a1320d
gogongkong <123641568+gogongkong@users.noreply.github.com>
2023-06-22 02:43:55 +0900

    Update 백준_12852번_DP_1로만들기2_S1_다른사람풀이.py

--- Month_06/Wk25/0621/백준_12852번_DP_1로만들기2_S1_다른사람풀이.py
+++ Month_06/Wk25/0621/백준_12852번_DP_1로만들기2_S1_다른사람풀이.py
@@ -4,29 +4,26 @@ https://www.acmicpc.net/problem/12852
 
 x = int(input())
 
-d = [0] * (x+1)
-
-result = [[1,i] for i in range(x+1)]
+d = [i for i in range(x+1)]
+d[1] = 0
+result = [i for i in range(x+1)]
+result[1] = 0
 
 for i in range(2,x+1):
     d[i] = d[i-1] + 1
-    
-    if i % 3 == 0:
-        d[i] = min(d[i], d[i // 3]+1)
+    result[i] = i-1
+    if i % 3 == 0 and d[i] > d[i //3] + 1:
+        d[i] = d[i//3] + 1
+        result[i] = i // 3
         
-    if i % 2 == 0:
-        d[i] = min(d[i], d[i // 2]+1)
+    if i % 2 == 0 and d[i] > d[i//2] + 1:
+        d[i] = d[i//2] + 1
+        result[i] = i // 2
         
 print(d[x])
+print(x, end=' ')
 
-result = [x]
-now = x
-temp = d[x] -1
-
-for i in range(x, 0, -1):
-    if d[i] == temp and (i + 1 == now or i *2 == now or i * 3 == now):
-        now = i
-        result.append(i)
-        temp -= 1
-
-print(*result)
+num = x
+while result[num] != 0:
+    print(result[num], end=' ')
+    num = result[num]
