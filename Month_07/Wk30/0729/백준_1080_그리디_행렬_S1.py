c9f0557808d41629428d5301e3f69f9fbd28ab82
gogongkong <zorek2@gmail.com>
2023-07-30 19:22:20 +0900

    pythonStudy_20230730
    C언어 공부하기 전 어려웠던 문제 다시 풀어보기

--- Month_07/Wk30/0729/백준_1080_그리디_행렬_S1.py
+++ Month_07/Wk30/0729/백준_1080_그리디_행렬_S1.py
@@ -4,18 +4,31 @@ https://www.acmicpc.net/problem/1080
 
 n,m = map(int, input().split())
 
-data = [list(int(input())) for _ in range(m)]
-result = [list(int(input())) for _ in range(m)]
+data = [list(map(int,input())) for _ in range(n)]
+result = [list(map(int,input())) for _ in range(n)]
 
 def convert(i,j):
     for x in range(i, i+3):
         for y in range(j, j+3):
             data[x][y] += 1 - data[x][y]
 
-first = 0
-sec = 0
+count = 0
 
-for i in range(m-2):
-    for j in range(n-2):
+for i in range(n-2):
+    for j in range(m-2):
+        if data[i][j] != result[i][j]: 
+            convert(i, j)
+            count +=1
 
-        
+check = 0
+
+for i in range(n):
+    for j in range(m):
+        if data[i][j] != result[i][j]:
+            check += 1
+            break
+
+if check == 1:
+    print(-1)
+else:
+    print(count)
