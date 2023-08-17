616389b6277e238e9f2ed36d216c7019cb0c5b02
gogongkong <zorek2@gmail.com>
2023-08-17 23:59:41 +0900

    Create 치킨쿠폰.py

---
+++ Month_08/Wk33/0817/치킨쿠폰.py
@@ -0,0 +1,32 @@
+'''
+https://school.programmers.co.kr/learn/courses/30/lessons/120884
+'''
+
+# 필요한 변수 정하기
+# 전체 치킨의 갯수
+# 치킨을 먹은 횟수(=정답)을 담을 변수 = result
+# 쿠폰으로 치킨을 먹고 남은 쿠폰의 갯수를 담을 변수 = cupon
+
+
+chicken = 1081
+
+def solution(chicken):
+    result = chicken // 10
+    cupon = (chicken // 10) + (chicken % 10)
+
+    # result += cupon // 10
+    # cupon = (cupon // 10) + (cupon % 10)
+
+    # result += cupon // 10
+    # cupon = (cupon // 10) + (cupon % 10)
+
+    # result += cupon // 10
+    # cupon = (cupon // 10) + (cupon % 10)
+
+    while cupon // 10 != 0:
+        result += cupon // 10
+        cupon = (cupon // 10) + (cupon % 10)
+
+    return print(result)
+
+solution(chicken)
